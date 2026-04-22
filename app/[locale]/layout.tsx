c0da72f2adf02626c71c530a08fc29fdc07c4fd2
Ricardo Rodriguez <ricardovelero@gmail.com>
2026-04-22 14:05:47 +0200

    feat: push footer down

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/footer';
 import Header from '@/components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import { NextIntlClientProvider } from 'next-intl';
@@ -19,7 +20,7 @@ export default async function RootLayout(props: Props) {
   return (
     <html suppressHydrationWarning lang={locale} className='scroll-smooth!'>
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 min-h-screen flex flex-col`}
       >
         <div className='bg-[#fbe2e3] absolute -top-24 -z-10 right-44 h-125 w-125 rounded-full blur-[10rem] sm:w-275 dark:bg-[#946263]'></div>
         <div className='bg-[#dbd7fb] absolute -top-4 -z-10 -left-140 h-125 w-200 rounded-full blur-[10rem] sm:w-275 md:-left-132 lg:-left-112 xl:-left-60 2xl:-left-20 dark:bg-[#676394]'></div>
@@ -31,8 +32,11 @@ export default async function RootLayout(props: Props) {
         >
           <ActiveSectionContextProvider>
             <NextIntlClientProvider locale={locale}>
-              <Header />
-              {children}
+              <div className='flex flex-col min-h-screen'>
+                <Header />
+                <main className='flex-1'>{children}</main>
+                <Footer />
+              </div>
             </NextIntlClientProvider>
           </ActiveSectionContextProvider>
         </ThemeProvider>
